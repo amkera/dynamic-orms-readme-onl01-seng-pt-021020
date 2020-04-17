c8845df43f37045b05641c51fb892a31508db8ea
Amber Kerawala <amber_kerawala@berkeley.edu>
2020-04-17 01:50:04 +0000

    Automatically backed up by Learn

--- config/environment.rb
+++ config/environment.rb
@@ -2,7 +2,11 @@ require 'sqlite3'
 
 
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
+#Creating the database 
+
+
 DB[:conn].execute("DROP TABLE IF EXISTS songs")
+#dropping the songs table to avoid an error
 
 sql = <<-SQL
   CREATE TABLE IF NOT EXISTS songs (
@@ -11,6 +15,7 @@ sql = <<-SQL
   album TEXT
   )
 SQL
+#Creating the songs table
 
 DB[:conn].execute(sql)
 DB[:conn].results_as_hash = true
